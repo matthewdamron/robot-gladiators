045135eb434d3c54cea5e80c74a9012cf0dacaa0
Matthew Damron <mattdamron@msn.com>
2020-11-09 20:49:51 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,6 +23,11 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
             enemyHealth = 50;
             fight(pickedEnemyName);
+
+            // if we're not at the last enemy in the array
+            if (i < enemyNames.length -1) {
+              shop();
+            }
         }
         else {
             window.alert("You have lost your robot in battle! Game Over!");
@@ -57,7 +62,50 @@ var endGame = function() {
 
 // create shop function
 var shop = function() {
+  console.log(playerName + " has " + playerMoney + " to spend.");
+  // ask player what they'd like to do
+  var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVEthe store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
 
+  // use switch to carry out action
+  switch (shopOptionPrompt.toLowerCase()) {
+    case "refill":
+      if (playerMoney >= 7) {
+        // refill players health
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        // increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+
+    case "upgrade":
+      if (playerMoney >= 7) {
+        // upgrade players attack
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        // increase attack and decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney -7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+      break;
+
+    // leave store
+    case "leave": window.alert("Leaving the store.");
+
+    // do nothing so function will end
+    break;
+
+    default: window.alert("You did not pick a valid option. Try again.");
+
+    // call shop() again to force player to pick a valid option
+    shop();
+    break;
+  }
 }
 
 // create fight function
@@ -67,7 +115,7 @@ var fight = function(enemyName) {
       var promptFight = window.prompt("Would you like FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
       // if player picks "skip" confirm and then stop the loop
-      if (promptFight.toLocaleLowerCase() === "skip") {
+      if (promptFight.toLowerCase() === "skip") {
         // confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
   
